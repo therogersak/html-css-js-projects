4b1df2abf2f9d6601c756606878a25bc4c85eb93
therogersak <therogersak@gmail.com>
2022-09-02 16:22:08 +0530

    initial commit

---
+++ otp_input_field/index.js
@@ -0,0 +1,53 @@
+const inputs = document.querySelectorAll('.otp-field input');
+
+inputs.forEach((input, index) => {
+    input.dataset.index = index;
+    input.addEventListener('paste', handleOnPasteOtp)
+    input.addEventListener('keyup', handleOtp)
+})
+
+
+function handleOnPasteOtp(e){
+    const data = e.clipboardData.getData('text');
+    const value = data.split('');
+    if(value.length === inputs.length){
+        inputs.forEach((input, index)=> (
+            input.value = value[index]
+        ))
+    }
+}
+
+
+function handleOtp(e){
+    const input = e.target;
+    let value = input.value;
+    input.value = "";
+    input.value = value ? value[0]: "";
+
+    let fieldIndex = input.dataset.index;
+    if(value.length > 0 && fieldIndex < inputs.length - 1){
+        input.nextElementSibling.focus();
+    }
+
+    if(e.key === "Backspace" && fieldIndex > 0){
+        input.previousElementSibling.focus()
+    }
+
+    if(fieldIndex = inputs.length -1){
+        submit()
+    }
+}
+
+function submit(){
+    console.log("submitting...");
+
+    let otp = "";
+
+    inputs.forEach((input) => {
+        opt += input.value;
+        input.disabled = true;
+        input.classList.add("disabled");
+    })
+    console.log(opt);
+    //call api below
+}
