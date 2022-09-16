6b2ae9a3c4b74262e64cb40c2f05d534bd55557c
therogersak <therogersak@gmail.com>
2022-09-16 17:34:16 +0530

    portfolio

---
+++ full-screen/index.js
@@ -0,0 +1,13 @@
+const img = document.getElementById("img");
+
+function fullScreen() {
+  if (img.requestFullscreen) {
+    img.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
